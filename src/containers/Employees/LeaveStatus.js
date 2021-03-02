68b6002a1189f49d3c77564195eb608e59d9a3bf
kavinda <kavindasr@icloud.com>
2021-03-03 03:29:26 +0530

    status

---
+++ src/containers/Employees/LeaveStatus.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import { connect } from 'react-redux';
+import {getLeaveStatus} from "../../api/Users"
+import Navbar from "../../components/Navbar/NavbarEmp"
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+import { DataGrid } from '@material-ui/data-grid';
+import Paper from '@material-ui/core/Paper';
+
+// grid
+
+// card
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+      },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        width: '100%',
+        color: 'blue',
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+        justifyItems: 'center'
+    },
+    card:{
+        width: '60%',
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        width: '100%',
+      },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+    button: {
+        marginTop: theme.spacing(2),
+    }
+}));
+
+const LeaveStatus = props =>  {
+    const classes = useStyles();
+    // const { addAlert } = props;
+    const [isLoading, setIsLoading] = useState(true);
+    const [leaveDetail, setLeaveDetail] =useState([{}]);
+
+
+    useEffect(() => {
+        if (isLoading) {
+            getLeaveStatus()
+            .then((response) => {
+              if (!response.error) {  
+                response.data.map(ob => {
+                  if(ob.is_approved === 1) ob.is_approved = "Accepted";
+                  else if(ob.is_approved === 0) ob.is_approved = "Rejected";
+                  else ob.is_approved = "Pendding...";
+                })
+                setLeaveDetail(response.data)
+                console.log(response.data)
+              }
+            })
+            .finally(() => setIsLoading(false));
+        }
+      }, [isLoading,props.employeeID]);
+    return (
+        <div className={classes.root}>
+            <Navbar/>
+            <Paper className={classes.paper}>    
+                <Card className={classes.card}>
+                    <CardHeader
+                        title="Leave application status"
+                        subheader="Jupyter Lab"
+                    />
+                    <CardContent>
+                    <div style={{ height: 250, width: '100%' }}>
+                        <DataGrid
+                            columns={[
+                            { field: 'Date' },
+                            { field: 'Leave type', width: 200 },
+                            { field: 'Status' },
+                            ]}
+                            rows={leaveDetail}
+                        />
+                        </div>
+                    </CardContent>
+                </Card>
+            </Paper>
+        </div>
+    );
+}
+
+const mapStateToProps = (state) => {
+  return {
+      error: state.auth.error,
+      loading: state.auth.loading,
+      isAuthenticated: state.auth.token != null,
+      authRedirectPath: state.auth.authRedirectPath,
+      employeeID:state.auth.employeeID,
+      isAdmin:state.auth.IsAdmin,
+      isHrm:state.auth.IsHrm,
+      IsSupervisor:state.auth.IsSupervisor,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LeaveStatus);
